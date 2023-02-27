2a7e4a0625ec1c3174f3159eae65fd71a95ea72b
Andrew Jones <ajones@digitalbazaar.com>
2023-02-27 11:41:16 -0600

    Create custom component for selecting VCs.

---
+++ components/CredentialSelect.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-checkbox
+    val="true"
+    @click="toggleSelect(id)">
+    <slot />
+  </q-checkbox>
+</template>
+
+<script>
+export default {
+  name: 'CredentialSelect',
+  methods: {
+    toggleSelect(id) {
+      if(this.selected.has(id)) {
+        this.selected.delete(id);
+        return;
+      }
+      this.selected.add(id);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
