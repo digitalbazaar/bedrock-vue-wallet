29c2e40ba4df11b078d45c0af346f27806edf53e
Andrew Jones <ajones@digitalbazaar.com>
2023-06-02 15:20:39 -0400

    Add padding to new spacer div & lint vue files.

--- components/WalletHeader.vue
+++ components/WalletHeader.vue
@@ -1,115 +1,115 @@
 <template>
   <q-toolbar class="bg-white q-pa-none">
-    <div id="mobile-test" class="row bg-primary full-width items-center no-wrap mobile-height">
-    <q-btn
-      v-show="account"
-      flat
-      round
-      dense
-      color="info"
-      icon="fa fa-bars"
-      class="toggle-drawer-btn lt-md"
-      @click="toggleDrawer()" />
-    <q-toolbar-title
-      class="row cursor-pointer"
-      @click="home()">
-      <img
-        v-if="branding.logo"
-        :src="branding.logo"
-        :style="$q.screen.lt.sm ? `height: ${branding.logoSize.mobile}` :
-          `height: ${branding.logoSize.desktop}`">
-      <div v-else>
-        {{branding.name}}
-      </div>
-    </q-toolbar-title>
-    <q-btn-toggle
-      v-if="account"
-      v-model="navRouteName"
-      color="accent"
-      toggle-color="info"
-      class="gt-sm"
-      flat
-      :options="[
-        {label: 'Credentials', value: 'home'},
-        {label: 'Profiles', value: 'profiles'}
-      ]"
-      @click="handleNav()" />
-    <div v-if="account">
+    <div class="row q-px-md bg-primary full-width items-center no-wrap mobile-height">
       <q-btn
-        v-if="$q.screen.lt.md"
+        v-show="account"
         flat
-        no-wrap
-        dense
-        class="persona-btn q-mr-sm"
-        @click="interact()">
-        <interact-icon
-          :fill="branding.brand.info" />
-      </q-btn>
-      <q-btn
-        flat
-        no-wrap
+        round
         dense
         color="info"
-        icon="fas fa-user-circle"
-        class="persona-btn">
-        <q-menu
-          anchor-click
-          fit
+        icon="fa fa-bars"
+        class="toggle-drawer-btn lt-md"
+        @click="toggleDrawer()" />
+      <q-toolbar-title
+        class="row cursor-pointer"
+        @click="home()">
+        <img
+          v-if="branding.logo"
+          :src="branding.logo"
+          :style="$q.screen.lt.sm ? `height: ${branding.logoSize.mobile}` :
+            `height: ${branding.logoSize.desktop}`">
+        <div v-else>
+          {{branding.name}}
+        </div>
+      </q-toolbar-title>
+      <q-btn-toggle
+        v-if="account"
+        v-model="navRouteName"
+        color="accent"
+        toggle-color="info"
+        class="gt-sm"
+        flat
+        :options="[
+          {label: 'Credentials', value: 'home'},
+          {label: 'Profiles', value: 'profiles'}
+        ]"
+        @click="handleNav()" />
+      <div v-if="account">
+        <q-btn
+          v-if="$q.screen.lt.md"
+          flat
           no-wrap
-          anchor="bottom right"
-          self="top right">
-          <q-separator />
-          <q-list
-            separator
-            link
-            class="text-subtitle1">
-            <q-item
-              v-close-popup
-              clickable
-              @click="settings()">
+          dense
+          class="persona-btn q-mr-sm"
+          @click="interact()">
+          <interact-icon
+            :fill="branding.brand.info" />
+        </q-btn>
+        <q-btn
+          flat
+          no-wrap
+          dense
+          color="info"
+          icon="fas fa-user-circle"
+          class="persona-btn">
+          <q-menu
+            anchor-click
+            fit
+            no-wrap
+            anchor="bottom right"
+            self="top right">
+            <q-separator />
+            <q-list
+              separator
+              link
+              class="text-subtitle1">
               <q-item
-                avatar
-                class="items-center">
-                <q-icon
-                  name="fas fa-cog"
-                  style="font-size: 25px;" />
+                v-close-popup
+                clickable
+                @click="settings()">
+                <q-item
+                  avatar
+                  class="items-center">
+                  <q-icon
+                    name="fas fa-cog"
+                    style="font-size: 25px;" />
+                </q-item>
+                <q-item-section>
+                  <q-item-label>
+                    Settings
+                  </q-item-label>
+                </q-item-section>
               </q-item>
-              <q-item-section>
-                <q-item-label>
-                  Settings
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-            <q-item
-              v-close-popup
-              clickable
-              @click="logout()">
               <q-item
-                avatar
-                class="items-center">
-                <q-icon
-                  name="fa fa-sign-out-alt"
-                  style="font-size: 25px;" />
+                v-close-popup
+                clickable
+                @click="logout()">
+                <q-item
+                  avatar
+                  class="items-center">
+                  <q-icon
+                    name="fa fa-sign-out-alt"
+                    style="font-size: 25px;" />
+                </q-item>
+                <q-item-section>
+                  <q-item-label>
+                    Log Out
+                  </q-item-label>
+                </q-item-section>
               </q-item>
-              <q-item-section>
-                <q-item-label>
-                  Log Out
-                </q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-menu>
-      </q-btn>
+            </q-list>
+          </q-menu>
+        </q-btn>
+      </div>
+      <q-btn
+        v-else
+        flat
+        no-wrap
+        color="info"
+        icon="fa fa-sign-in-alt"
+        label="Log in"
+        @click="login()" />
     </div>
-    <q-btn
-      v-else
-      flat
-      no-wrap
-      color="info"
-      icon="fa fa-sign-in-alt"
-      label="Log in"
-      @click="login()" />
-  </div>
   </q-toolbar>
 </template>
 
