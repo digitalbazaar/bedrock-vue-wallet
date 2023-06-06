59a773788759b509ff83b56f9754ec0cd862f915
David I. Lehn <dlehn@digitalbazaar.com>
2023-06-05 21:56:46 -0400

    Add minimal import test.
    Add a webpack test of importing main exports.

--- test/web/10-api.js
+++ test/web/10-api.js
@@ -1,6 +1,9 @@
 /*!
- * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Digital Bazaar, Inc. All rights reserved.
  */
+/* eslint-disable-next-line no-unused-vars */
+import * as VueWallet from '@bedrock/vue-wallet';
+
 describe('FIXME API', function() {
   describe('FIXME API', function() {
     it('should test something', async () => {
