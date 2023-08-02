0153961f26389dde31d88fcc04dc723b9a262d59
Dave Longley <dlongley@digitalbazaar.com>
2023-08-02 01:41:00 -0400

    Add `ChapiHeader` component.

---
+++ components/ChapiHeader.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="row justify-center"
+    :class="$q.screen.lt.sm ? 'q-px-lg' : ''">
+    <div class="column">
+      <div class="q-mt-md text-center">
+        <img
+          :src="image"
+          class="s-rp-image">
+      </div>
+      <h6 class="q-my-sm">
+        <strong>{{name}}</strong> would like to interact with you.
+      </h6>
+    </div>
+  </div>
+</template>
+
+<script>
+/*!
+ * Copyright (c) 2019-2023 Digital Bazaar, Inc. All rights reserved.
+ */
+export default {
+  name: 'ChapiHeader',
+  props: {
+    image: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+.s-rp-image {
+  width: 64px;
+  height: 64px;
+}
+</style>
