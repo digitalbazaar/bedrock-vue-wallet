d1f0dafab3e03d3bc49f74f0e110bb223f815278
David I. Lehn <dlehn@digitalbazaar.com>
2023-08-03 17:38:35 -0400

    Use vue3 eslint config.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     'digitalbazaar',
     'digitalbazaar/jsdoc',
-    'digitalbazaar/vue'
+    'digitalbazaar/vue3'
   ],
   rules: {
     'vue/multi-word-component-names': 0
