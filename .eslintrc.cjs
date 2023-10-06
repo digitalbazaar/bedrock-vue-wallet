342ab98082e645d083a311b95c11d9f8c6ab6172
David I. Lehn <dlehn@digitalbazaar.com>
2023-10-05 21:04:26 -0400

    Update eslint config.
    - Parser version setup in digitalbazaar config.
    - quasar plugin setup in quasar config.
    - Put quasar config first so digitalbazaar configs can override it.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,14 @@
 module.exports = {
   root: true,
-  parserOptions: {
-    // this is required for dynamic import()
-    ecmaVersion: 2020
-  },
   env: {
     browser: true,
     node: true
   },
-  plugins: [
-    'quasar'
-  ],
   extends: [
+    'plugin:quasar/standard',
     'digitalbazaar',
     'digitalbazaar/jsdoc',
-    'digitalbazaar/vue3',
-    'plugin:quasar/standard'
+    'digitalbazaar/vue3'
   ],
   rules: {},
   ignorePatterns: ['node_modules/']
